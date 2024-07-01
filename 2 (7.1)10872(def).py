11dc4f812a6588aefced51131cfe8ed11b7fcc28
Gregor3236 <135096619+Gregor3236@users.noreply.github.com>
2024-07-01 19:47:31 +0900

    Update 2 (7.1)10872(def).py

--- 2 (7.1)10872(def).py
+++ 2 (7.1)10872(def).py
@@ -1,9 +1,1 @@
-num = int(input()) # 숫자 입력
 
-def factorial(num): # factorial 함수 정의
-  if num <= 1: # num의 값이 1 이하
-    return 1 # 1 반환
-  else: # 나머지 경우
-    return num * factorial(num - 1) # 함수 리콜
-
-print(factorial(num))
